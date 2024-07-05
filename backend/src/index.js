f96c25dc719dbff4c10775c6dd5344de2b0f19cf
hamza benarfa <hamzabenarfa4@gmail.com>
2024-07-05 18:10:38 +0100

    works to do

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,3 +13,22 @@ app.listen(port, () =>
     console.log(`
   🚀 Server ready at: http://localhost:${port}`),
   );
+
+
+
+  /**
+   * 1-create sujet
+   * 2-candiat : ychouf les sujet l koll : get all
+   *  bch y5tar sujet li 7chtou bih  : get by id 
+   * 
+   *  bch y3Ml postuler :  userId  sujetId 
+   * 
+   * 3-admin : ychouf il liste t3 condudature
+   * 
+   *  ADMIN: ynjm yfs5 sujet , ymodifi sujet ,
+   *      
+   * 
+   * 4-admin : accepter , refuser  ::: PUT
+   * 
+   * 
+   */
--- backend/src/routes/sujet.js
+++ backend/src/routes/sujet.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const {createSujet} = require('../controller/sujet')
 
-//router.get('/',getAllSujet);
+// router.get('/',getAllSujet);
+// router.get('/:id',getSujetById);
 
 router.post('/',createSujet);
+
 
 module.exports = router;
